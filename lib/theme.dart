011360f8ac54f41ebfa5d9e1412117302c4b17ed
boat.suka <boat.suka@gmail.com>
2021-09-07 13:19:17 +0700

    build theme color project.

---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color kPrepareColor = Colors.orange;
+  static const Color kServedColor = Colors.green;
+  static const Color kCanceledColor = Colors.red;
+  static const Color kPrimaryColor = Color(0xff031525);
+  static const Color kUnselectedItemColor = Color(0xff8ea1b9);
+  static const Color kAccentColor = Color(0xffff8f00);
+  static const Color kGreyText = Color(0xffcdd2de);
+}
